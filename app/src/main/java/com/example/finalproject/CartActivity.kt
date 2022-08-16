2e0a57d5973a2e3e8b4a0b0decbb262218abb87c
ArwaHazem <arhazem93@gmail.com>
2022-08-16 15:49:01 +0200

    bottom nav bar in Cart,UserProfile,ProductList Activities

--- app/src/main/java/com/example/finalproject/CartActivity.kt
+++ app/src/main/java/com/example/finalproject/CartActivity.kt
@@ -1,11 +1,38 @@
 package com.example.finalproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class CartActivity : AppCompatActivity() {
+class CartActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNavigationView.selectedItemId = R.id.ic_cart
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.ic_cart ->{
+                return true
+            }
+            R.id.ic_profile ->{
+                startActivity(Intent(this,UserProfileActivity::class.java))
+                overridePendingTransition(0,0)
+                return true
+            }
+            R.id.ic_home ->{
+                startActivity(Intent(this,ProductListActivity::class.java))
+                overridePendingTransition(0,0)
+                return true
+            }
+            else ->{
+                return false
+            }
+        }
     }
 }
--- app/src/main/java/com/example/finalproject/UserProfileActivity.kt
+++ app/src/main/java/com/example/finalproject/UserProfileActivity.kt
@@ -5,14 +5,16 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.ActionBar
 import com.bumptech.glide.Glide
 import com.example.finalproject.sharedpref.SharedPre
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class UserProfileActivity : AppCompatActivity() {
+class UserProfileActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     lateinit var image : ImageView
     lateinit var userEmail:TextView
     lateinit var userName:TextView
@@ -25,9 +27,14 @@ class UserProfileActivity : AppCompatActivity() {
         userEmail = findViewById(R.id.user_email)
         userName = findViewById(R.id.username)
         logOut = findViewById(R.id.logout)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNavigationView.selectedItemId = R.id.ic_profile
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
 
         val urlToImg = "https://th.bing.com/th/id/R.fe20a57e77099fe1baea254d50f6802c?rik=FsyFE8G2RLe1EA&riu=http%3a%2f%2fehonami.blob.core.windows.net%2fmedia%2f2014%2f11%2fcoffee-even-decaf-found-benefit-liver-health.jpg&ehk=XtVvJ0uvsrpSLM02RVuUPBj0EQvfbEYoC7lew1%2bWrmk%3d&risl=&pid=ImgRaw&r=0"
         Glide.with(this).load(urlToImg).into(image)
+
+
 
         logOut.setOnClickListener {
             SharedPre.setText("")
@@ -36,6 +43,26 @@ class UserProfileActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.ic_cart ->{
+                startActivity(Intent(this,CartActivity::class.java))
+                overridePendingTransition(0,0)
+                return true
+            }
+            R.id.ic_profile ->{
+                return true
+            }
+            R.id.ic_home ->{
+                startActivity(Intent(this,ProductListActivity::class.java))
+                overridePendingTransition(0,0)
+                return true
+            }
+            else ->{
+                return false
+            }
+        }
     }
 
 }
